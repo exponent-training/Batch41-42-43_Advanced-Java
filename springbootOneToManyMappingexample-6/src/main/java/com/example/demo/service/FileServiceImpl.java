f1b0a760c2003383a5dd81107ab77bbcf6343cd1
zaid123 <jaidshaikh408@gmail.com>
2024-07-26 19:49:16 +0530

    File upload on server side.

--- springbootOneToManyMappingexample-6/src/main/java/com/example/demo/service/FileServiceImpl.java
+++ springbootOneToManyMappingexample-6/src/main/java/com/example/demo/service/FileServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.demo.service;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,25 +16,48 @@ import com.example.demo.repo.FileRepository;
 @Service
 public class FileServiceImpl implements FileServicei{
 
-	
+
 	@Autowired
 	private FileRepository fileRepository;
-	
+
 	@Override
 	public void uploadFile(MultipartFile file) {
 		// TODO Auto-generated method stub
 		FileData fileData = new FileData();
-		fileData.setFileName(file.getOriginalFilename());
-		fileData.setFileType(file.getContentType());
-		fileData.setFileSize(file.getSize());
+
 		try {
+			fileUploadOnServerPath(file.getBytes(),file.getOriginalFilename());
+			fileData.setFileName(file.getOriginalFilename());
+			fileData.setFileType(file.getContentType());
+			fileData.setFileSize(file.getSize());
 			fileData.setFileData(file.getBytes());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		fileRepository.save(fileData);
+		//fileRepository.save(fileData);
+
+	}
+
+
+	private String fileUploadOnServerPath(byte[] filedata,String filename) {
+
+		int i = filename.lastIndexOf(".");
+	
+        String ext = filename.substring(i);
+        
+		System.out.println(ext);
 		
+		Path path = Paths.get("D:"+File.separator+"Demo"+File.separator+"public"+ext);
+
+		try {
+			Files.write(path, filedata);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return "File Writing done on server";
 	}
 
 }
